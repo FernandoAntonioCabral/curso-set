ca07e0aff091ae04f4780288a0a053c60e52b796
Fernando Cabral <fernandoantoniojp10@gmail.com>
2023-04-27 16:55:54 -0300

    Adicionando projeto basico do curso sobre Set

---
+++ Course/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public int UniCod { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UniCod.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is User)) {
+                return false;
+            }
+            User other = obj as User;
+            return UniCod.Equals(other.UniCod);
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,29 @@
+﻿using Course.Entities;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<User> set = new HashSet<User>();
+
+            Console.Write("How many courses the instructor have? ");
+            int x = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= x; i++)
+            {
+                Console.Write($"How many students for course {i}? ");
+                int n = int.Parse(Console.ReadLine());
+                for (int g = 1; g <= n; g++)
+                {
+                    set.Add(new User { UniCod = int.Parse(Console.ReadLine()) });
+                }
+            
+            
+            
+            }
+            Console.Write("Total students: " + set.Count);
+        }
+    }
+}
